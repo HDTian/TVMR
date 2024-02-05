a9d741203b22c385a8b971aa5c130381edd964ef
HDTian <haodongt1123@gmail.com>
2024-02-05 15:40:31 +0000

    pre_real

---
+++ sim_real_illustration/pre_real.R
@@ -0,0 +1,12 @@
+
+
+###pre-real script: clean the data for formal real analysis (data stored in the folder Data in Git)
+
+library(data.table)#as.data.table()
+library(fdapace)#PACE
+library(tidyverse) #一些data.table %>% 操作
+library(corrplot)#correlated SNPs
+
+
+
+
