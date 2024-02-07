9243118f9f57d8861e59993a65db0dd080f9d9ab
HDTian <haodongt1123@gmail.com>
2024-02-07 16:01:51 +0000

    real analysis: pre_real + formal_real

---
+++ sim_real_illustration/formal_real.R
@@ -0,0 +1,305 @@
+#formal_real
+
+
+library(data.table)#as.data.table()
+library(fdapace)#PACE
+library(tidyverse) #一些data.table %>% 操作
+library(corrplot)#correlated SNPs
+library(parallel)
+
+
+
+Dat<-read.csv(paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/Dat.csv'))
+Dat_n<-read.csv(paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/Dat_n.csv'))
+DatY<-read.csv(paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/DatY.csv'))
+
+###clarification
+#Dat1: reference data (no any initial selection)
+#Dat2: genotype data for SBP (258 nearly independent SNPs)
+#Dat3: longitudinal SBP data (保留一个小数点)
+#Dat4: urea data with measured timepoints
+
+#Dat_n是Dat1 2的结合：即 （G X）data （for non-time-varying UVMR）
+#DatY是Dat1 2 4的结合： 即 （G Y）data
+
+#Dat是Dat1 2 3的结合：即 （G PCs）data
+
+#Dat + DatY for MPCMR fitting (either one-sample individual level or two-sample summary level)
+
+
+
+###UVMR---------------------------------------------------------------
+dim(Dat_n)#367283    260  # ID + 258 SNPs + SBP(measured at study baseline) ; ID is called 'V1' ;SBP is called 'V2'
+dim(DatY)#350223    461  # ID + 258SNPs + urea + timepoint + nuisance phenotypes
+
+#individual-level data
+#can use either the one-sample fitting (2SLS or IVW; IVW==2SLS) or overlapping.two--data fitting (IVW)
+#their results should be very similar
+
+#only use naive IVW (or 2SLS) fitting: do not consider the uncertainty of the G-X association estimates
+
+
+#combine Dat_n and DatY into one-sample individual data
+
+
+
+ID<-match(  Dat_n$V1, DatY$V1 )  #按照前者的顺序
+DatY_<-DatY[ID,]
+Dat_uvmr<-cbind(  Dat_n,  DatY_[,260]  )  #names(DatY_)[260]=="Y"
+dim(Dat_uvmr) #367283    261
+ID<-  apply(  is.na(Dat_uvmr[,1:261]  )  ,  1, sum  )==0  #remove NA individuals
+Dat_UVMR<-Dat_uvmr[ID,]
+dim(Dat_UVMR) #349883    261 # ID + 258 SNPs + SBP (V2) + urea (Y)
+names( Dat_UVMR)[261]<-'Y'
+
+#release some store spaces in the global environment
+rm( Dat_uvmr, Dat12, Dat123, Dat124 )
+
+
+###naive IVW---------------
+fitGX<-lm( Dat_UVMR$V2 ~  as.matrix( Dat_UVMR[,2:259])   )
+bx<-as.numeric(summary(fitGX)$coef[-1,1]) ;  bxse<-as.numeric(summary(fitGX)$coef[-1,2])
+BX<-bx; BXse<-bxse #UVMR  so one-dimensional
+fitGY<-lm( Dat_UVMR$Y ~  as.matrix( Dat_UVMR[,2:259] )  )
+by<-as.numeric(summary(fitGY)$coef[-1,1]) ;  byse<-as.numeric(summary(fitGY)$coef[-1,2])
+
+
+###my WLR(weigthed linear regression) code
+J<-258 # number of SNPs
+S<-vcov(fitGY  )[-1,-1]  #variance-covariance matrix
+#point estimates
+UVMRest<-solve(    t(BX)%*%solve( S )%*%BX            )%*%t(BX)%*%solve(S)%*%by
+##random-effect setting
+tau2<-max(1,   t(by- BX%*%UVMRest)%*%solve( S )%*%(by- BX%*%UVMRest)/(J-length(UVMRest))      )
+
+#variance matrix of the IVW-regression estimator
+UVMRvar<-solve(    t(BX)%*%solve( S )%*%BX            )*tau2
+
+#estimate and 95% CI
+as.numeric(UVMRest);  c(UVMRest)+c(-1,1)*1.96*sqrt( c(UVMRvar) )
+
+# 0.00815089
+# 0.004409627 0.011892153 (urea)
+
+###Steve's package
+library(MendelianRandomization)
+mr_ivw(mr_input(bx, bxse, by, byse))
+
+#Inverse-variance weighted method
+#(variants uncorrelated, random-effect model)
+
+#Number of Variants : 258
+
+#------------------------------------------------------------------
+#  Method Estimate Std Error 95% CI       p-value
+#IVW    0.008     0.002 0.004, 0.012   0.000
+#------------------------------------------------------------------
+#  Residual standard error =  2.175
+#Heterogeneity test statistic (Cochran's Q) = 1215.9872 on 257 degrees of freedom, (p-value = 0.0000). I^2 = 78.9%.
+#F statistic = 28.6.
+
+
+###MR scatterplot
+BX<-cbind(bx,by)
+BXse<-cbind(bxse,byse)
+ggdata<-data.frame(   beta1=BX[,1], beta2=BX[,2],
+                      beta1_error_up=BX[,1]+1.96*BXse[,1], beta1_error_low=BX[,1]-1.96*BXse[,1] ,
+                      beta2_error_up=BX[,2]+1.96*BXse[,2], beta2_error_low=BX[,2]-1.96*BXse[,2])
+scatterp<-     ggplot(ggdata, aes(beta1, beta2))+
+  geom_errorbar(data=ggdata,mapping=aes(x=beta1,ymin=beta2_error_low,ymax=beta2_error_up),width = 0,color='#666666')+
+  geom_errorbar(data=ggdata,mapping=aes(y=beta2,xmin=beta1_error_low,xmax=beta1_error_up),width = 0,color='#666666')+
+  geom_point(data=ggdata,mapping=aes(x=beta1,y=beta2),size=1.8)+
+  geom_hline(yintercept = 0,linetype='dashed',col='#999999')+
+  geom_vline(xintercept = 0,linetype='dashed',col='#999999')+
+  #geom_abline(intercept=0,slope=1,linetype='dashed',col='#999999')+
+  labs(x = 'Genetic association with the exposure', y = 'Genetic association with the outcome')+
+  theme_bw()+theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())
+scatterp
+#save as: UVMR_scatterplot_urea
+
+ggsave(paste0('UVMR_scatterplot_urea_new','.eps' ),
+       plot = scatterp,
+       path=paste0('/Users/haodongtian/Documents/TVMR(latex)/plots/'),
+       height = 8, width = 8, units = "in",limitsize=TRUE)
+
+
+
+
+###MPCMR fitting--------------------------------------------------------------------------------
+###---------------------------------------------------------------------------------------------
+
+
+dim(Dat)  #116378   460  # ID + 258SNPs + Xt(the string for longitudinal information)  + nuisance phenotypes
+dim(DatY) #350223   461  # ID + 258SNPs +         urea  + timepoint                    + nuisance phenotypes
+
+
+
+###use the partial time region------------------------------
+get_exposure_and_time<-function(s){#string item
+  v<-as.numeric(strsplit(  s, '_'   )[[1]])
+  res<-list(Ly=v[ 1: (length(v)/2) ] , Lt=v[ -(1: (length(v)/2)) ]  )
+  return(res)
+}
+
+
+#the maximal time region
+leftrange<-0   ; rightrange<-1000
+Ly_real<-list()
+Lt_real<-list()
+ID<-rep(NA, nrow(Dat))  #用来记录那些individual没有落入需要的time region里，方便排除
+for(i in 1:nrow(Dat)){
+  res<-get_exposure_and_time( Dat$Xt[i])
+  ts<-res$Lt  ; ys<-res$Ly
+  #肯定是存在至少一个SBP measurement for all individual
+  which_in_sub<-(leftrange<=ts)&(ts<=rightrange)
+  Ly_real[[i]]<-ys[which_in_sub]
+  Lt_real[[i]]<-ts[which_in_sub]
+  ID[i]<-  sum(which_in_sub)>0
+}
+CreateDesignPlot(Lt_real) #很完美的design plot
+#design_plot_0_1000  #500 500
+
+
+#a propriate sub time region such that FCPA can well work (a time gap = 25是最大的时间范围同时使得design plot很好)
+leftrange<-50   ; rightrange<-75
+Ly_real<-list()
+Lt_real<-list()
+ID<-rep(NA, nrow(Dat))  #用来记录那些individual没有落入需要的time region里，方便排除
+for(i in 1:nrow(Dat)){
+  res<-get_exposure_and_time( Dat$Xt[i])
+  ts<-res$Lt  ; ys<-res$Ly
+  #肯定是存在至少一个SBP measurement for all individual
+  which_in_sub<-(leftrange<=ts)&(ts<=rightrange)
+  Ly_real[[i]]<-ys[which_in_sub]
+  Lt_real[[i]]<-ts[which_in_sub]
+  ID[i]<-  sum(which_in_sub)>0
+}
+CreateDesignPlot(Lt_real) #很完美的design plot
+#design_plot_50_75  #500 500
+
+
+##先移除out-of-region的NA项，合成一个Dat_sub,再做fPCA
+
+Dat_sub<-Dat[ID,]
+dim(Dat_sub) #106498   460
+
+Lt_real_sub<-Lt_real[ID] ; Ly_real_sub<-Ly_real[ID]
+length( Lt_real_sub  ); length( Ly_real_sub  )  # 106498  #106498
+
+###FPCA----------------------------------------------------------------------------------
+
+#memory release: only maintain Dat, DatY, Dat_sub, Lt_real_sub, Ly_real_sub
+all_variables <- ls()
+variables_to_keep <- c("Dat", "DatY" , "Dat_sub" , "Lt_real_sub" , "Ly_real_sub")
+variables_to_remove <- setdiff(all_variables, variables_to_keep)
+rm(list = variables_to_remove)
+
+###FPCA
+res <- FPCA(Ly_real_sub, Lt_real_sub,list(dataType='Sparse', error=TRUE, verbose=TRUE))#默认的就是dataType='Sparse'
+#time cost: ～2 mins
+
+
+
+saveRDS(res,file=paste0("/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/",50,"_",75,".RData"))
+#res<-readRDS(paste0("/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/50_75.RData"))
+
+###review:
+#res$selectK: K the number of PC remained
+#res$lambda: the eigenvalues  (K = length(res$lambda) )
+#res$cumFVE: the cumulative FVE ( res$cumFVE ~= res$lambda/sum(res$lambda )  )
+
+#res$workGrid: length fixed to 51; used for present the values of the eigenfunction
+#res$obsGrid: all observed timepoint from the original data (50.0 50.1 50.2 50.3 ... 74.8 74.9 75.0)
+
+#res$xiEst: individual estimated PCs matrix ( res$xiVar: a list of length n, each of which indicates the variance of the estimated PCs)
+#res$phi: the 51*K matrix: the eigenfunction values at workGrid
+
+#quick see the values eg: sum(res$phi[,2]*res$phi[,3])
+#t(res$phi)%*%(res$phi)  #非对角线元素都等于0  good!
+
+
+##eigenfunctions
+plotEifun(res) #易理解: 越往后的PC对应的eigenfunction越浮夸(波动多)
+#the first 4 PCs just explain 95% FVE
+
+
+##individual fitted curve vs measured values over the selected time region
+PC<-res$xiEst
+dim(res$xiEst)  #106498     6
+
+plot_individual_exposure_curve<-function( iindex ){
+  plot(  Lt_real_sub[[iindex]],Ly_real_sub[[iindex]] ,
+         xlim=c(50,75) , ylim=c(80,160), col='red',main=iindex , xlab='Age' , ylab='SBP' )
+  lines( res$workGrid,     (res$phi)%*%PC[iindex,]+res$mu   )
+}
+plot_individual_exposure_curve(1)  #1 = iidex 随便选的 358也行
+
+
+
+###MPCMR fitting---------------------------------------------------------------
+
+#review: 258SNPs
+nrow(Dat_sub)==nrow(res$xiEst) #TRUE #确保(G X)data都做了fPCA
+dim(Dat_sub) #106498 (for 50_75)   460  #ID + 258SNPs + Xt(the string for longitudinal information)  + nuisance phenotypes
+
+
+#outcome time region selection (maybe no restriction at all to increae the sample size)
+leftrangeY<-0;rightrangeY<-100
+DatY_sub<-DatY[   (leftrangeY<DatY$age)&(DatY$age<rightrangeY)   ,   ]
+dim(DatY_sub) #350223    461
+
+
+###store some data, which can be directly used for MPCMR fitting - this is convenient for HPC
+write.csv(Dat_sub  , paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/Dat_sub.csv'), row.names=F)
+write.csv(DatY_sub  , paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/DatY_sub.csv'), row.names=F)
+
+###IDmatch (for one-sample individual data fitting)
+IDmatch_used<-match(Dat_sub$V1, DatY_sub$V1   )  #以Gmatrix为主导
+
+sum(  !is.na(IDmatch_used)  )  #101279 ##the overlapping sample size
+
+
+
+###MPCMR_GMM (one-sample individual-level data fitting)
+
+
+
+
+#basisfunction = the first two eigenfunctions (equivalent to just use the first 2 PCs; ie nPC=2)
+print(    Sys.time()  )
+MPCMR_GMM_real<-MPCMR_GMM(    Gmatrix=Dat_sub[,2:259],
+                              res=res,
+                              Yvector=DatY_sub$Y,
+                              Gymatrix=DatY_sub[,2:259],
+                              IDmatch=IDmatch_used,
+                              nPC=2,  ##how many nPC to be retained?
+                              nL=NA,  ##how many polynomial basisfunctions used for fitting? only acailable for <= nPC; the default is = nPC
+                              eigenfit=TRUE,
+                              polyfit=FALSE,  #no basisfunction fitting to save time
+                              LMCI=TRUE,
+                              LMCI2=TRUE,
+                              nLM=20, #with parallel
+                              Parallel=TRUE, #默认=TRUE 且使用detectCores()-1 cores
+                              XYmodel= NA
+                         )
+print(    Sys.time()  )
+
+
+
+###Note: if MPCMR fitting is too time-consuming, use HPC to fit instead
+
+#MPCMR_GMM_real<-
+
+
+###result analysis
+
+##weak instrument analysis
+MPCMR_GMM_real$ISres
+
+
+
+
+
+
+
+
--- sim_real_illustration/pre_real.R
+++ sim_real_illustration/pre_real.R
@@ -8,5 +8,195 @@ library(tidyverse) #一些data.table %>% 操作
 library(corrplot)#correlated SNPs
 
 
+###The 4 dataset:(各自的quality control和特殊处理)
+
+###Dat1: ID reference and covariate dataset--------------------------------------------------
+master<-read.csv('/Users/haodongtian/Documents/files/R new/MPCMR/master.csv')
+dtp<-as.data.table(master)
+
+###no any initial selection (to increase the sample size)
+#dtp<-dtp[dtp$euro==1,]
+#dtp<-dtp[dtp$race=='White',]
+#dtp<-dtp[dtp$sex=='Female',]
+
+Dat1<-dtp
+
+dim(Dat1) #488366    204
+write.csv(Dat1  , paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/Dat1.csv'), row.names=F)
+#Dat1<-read.csv(paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/Dat1.csv'))
+
+###Dat2: genetic information dataset---------------------------------------------------------
+hannah_bp_euro<-read.csv('/Users/haodongtian/Documents/files/R new/MPCMR/hannah_bp_euro.csv')
+dtg<-as.data.table(hannah_bp_euro)
+#remove missing data + round SNP values + highly-correlated SNPs check
+dtg<-na.omit(dtg)
+dtg<-cbind(dtg[,1], round(    dtg[,   -1 ]      ))
+table(  as.vector(as.matrix(dtg[,-1]) ))    #check SNP values 0,1,2  #checked
+sort( as.numeric(cor( dtg[,   -1 ] ) ) , decreasing = TRUE  )[    (ncol(dtg[,   -1 ]) - 2): (ncol(dtg[,   -1 ]) - 2+10)    ]
+#前3项必是1.000000 #第4项很关键：就是max cor     #max cor=0.3    so r^2~= 0.09 < 0.2 the common threshold
+#其实全打入即使SNPs相关也不要紧；只要保证design matrix不要太奇异了即可
+#但是如果使用Q statistics; 最好使用independent SNPs for better joint (profile) likelihood maximization
+Dat2<-dtg
+
+dim(Dat2) #367643    259= ID + 258 SNPs
+write.csv(Dat2  , paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/Dat2.csv'), row.names=F)
+#Dat2<-read.csv(paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/Dat2.csv'))
+
+###Dat3: longitudinal exposure dataset---------------------------------------------------------
+load("/Users/haodongtian/Documents/files/R new/MPCMR/ukb_gp_qrisk_SB.rdata")
+Dat3_sbp<-ukb_gp_qrisk[ ukb_gp_qrisk$exposure_type =='sbp', ] #only focus on sbp as the exposure
+
+#coarsened + exposure mean; final returned as a string
+getString<-function( exposures,
+                     times ,
+                     coarsend_level=1   #level 使用1好了（而不是0），即保留一个小数点
+){
+  ctimes<-round(  times , coarsend_level )  #coarsened times
+  dat<-data.frame( X= exposures, time= ctimes     )
+  dat_mean<- dat %>% group_by( time) %>%  summarise(mean(X)) #group下的分类求mean/sort等操作
+  single_string<-paste(c(dat_mean$`mean(X)` ,   dat_mean$time),collapse='_')
+  return(single_string  )
+}
+
+Dat3<- Dat3_sbp %>% group_by( idno) %>%  summarise(getString(exposure_value,exposure_age)) #group下的分类求mean/sort等操作
+
+dim(Dat3)#158310      2
+
+#checked: Dat3没问题
+write.csv(Dat3  , paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/Dat3.csv'), row.names=F)
+#Dat3<-read.csv(paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/Dat3.csv'))
+
+###Dat4: outcome dataset-------------------------------------------------------------------
+#关于标号所对应的信息，参考https://biobank.ctsu.ox.ac.uk/crystal/field.cgi?id=30670
+#修改后面的id=*信息就好, 至于30670.0.0  30670.1.0 这种，其实是Instance的标号,往往用0就行，因为这个instance样本数量最多
+all_biomarker<-read.csv('/Users/haodongtian/Documents/files/R new/MPCMR/all_biomarker_variables.csv')
+
+##creatinine = all_biomarker$"30700-0.0"   #measurement date = all_biomarker$"30701-0.0"
+#outcome_dat<-cbind( all_biomarker$eid, all_biomarker$X30700.0.0, all_biomarker$X30701.0.0 )
+
+##urea = all_biomarker$"30670-0.0"  #measurement date = all_biomarker$"30671-0.0"
+outcome_dat<-cbind( all_biomarker$eid, all_biomarker$X30670.0.0, all_biomarker$X30671.0.0 )
+
+#match the dob (date of birth) values according to master reference dataset
+dpt_index<-match( outcome_dat[,1], master$Adiposity_sample_ID    )
+outcome_dat<-cbind(outcome_dat, master$dob[dpt_index]  )
+
+outcome_dat<-na.omit( outcome_dat)
+
+get_ages<-function( dom , dob   ){ #dom: date of measurement ;   dob: date of birth
+  DOM<-matrix( as.numeric(unlist(strsplit( dom , '-'   )  )) , length( dom )   , 3,byrow = TRUE ) #3= 年 - 月 -日
+  DOB<-matrix( as.numeric(unlist(strsplit( dob , '-'   )  )) , length( dob )   , 3,byrow = TRUE )
+  age<- DOM%*%c( 1, 1/12 , 1/365  ) - DOB%*%c( 1, 1/12 , 1/365  )
+  return(age)
+}
+outcome_dat<-cbind( outcome_dat , get_ages(outcome_dat[,3],outcome_dat[,4])      )
+
+Dat4<-data.frame( eid=outcome_dat[,1] , Y=outcome_dat[,2] , age=outcome_dat[,5] )
+
+dim(Dat4)  #465031      3 #ID + urea + timepoint
+
+write.csv(Dat4  , paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/Dat4.csv'), row.names=F)
+#Dat4<-read.csv(paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/Dat4.csv'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###data combination
+
+
+
+
+###------------------------------------------------------------------------------------------
+###One-sample individual data: (instruments exposure) dataset, combining Dat1+Dat2+Dat3->Dat123 ----------------------------
+
+#review:     match(c(3,1,5,6) , c(4,5,3,9) ) # 3 NA  2 NA
+#Dat1 交 Dat2 -> Dat12------------------------------------------------
+ID<-match(  Dat1$UKB_sample_ID, Dat2$X )  #按照前者的顺序
+
+Dat2_<-Dat2[ID,]
+Dat12<-cbind(  Dat1$Adiposity_sample_ID ,  Dat2_[,-1]  , Dat1[,-(1:4)]  )  #ID + 258 SNPs + nuisance phenotype variables
+names(Dat12)[1]<-'V1'
+ncol( Dat2_[,-1] )  # 258 SNPs
+
+###Dat12存储一下， 主要是为了后面的naive MR analysis (naive是指不考虑time-varying)
+Dat_n<-cbind(Dat12[,1:259],Dat12$sbp)  #dim(Dat_n) #197128    260
+#removes (ID,Gs,Xt) NA items
+ID<-  apply(  is.na(Dat_n[,1:260]  )  ,  1, sum  )==0
+Dat_n<-Dat_n[ID,]
+dim(Dat_n) #367283        260  #ID + 258 SNPs + SBP(measured at study baseline)
+names(Dat_n)[260]<-'V2'
+write.csv(Dat_n  , paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/Dat_n.csv'), row.names=F)
+#Dat_n<-read.csv(paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/Dat_n.csv'))
+
+
+
+#Dat12 交 Dat3 -> Dat123 ----------------------------------------------
+ID<-match(  Dat12$V1, as.numeric(Dat3$idno) )
+
+Dat3_<-Dat3[ID,]
+Dat123<-cbind(  Dat12[,1:259]  ,  Dat3_[,2]  ,   Dat12[,-c(1:259)])
+
+#Dat123 remove missing data -> storage as Dat---------------------------
+dim(Dat123)  #488366    460
+#names(Dat123)[260]
+names(Dat123)[260]<-'Xt'
+
+#removes (ID,Gs,Xt) NA items
+ID<-  apply(  is.na(Dat123[,1:260]  )  ,  1, sum  )==0
+Dat<-Dat123[ID,]
+
+
+dim(Dat)  #116378    460
+
+write.csv(Dat  , paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/Dat.csv'), row.names=F)
+#Dat<-read.csv(paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/Dat.csv'))
+
+nrow( na.omit(Dat[,1:260]))==nrow(Dat)#TRUE
+
+
+
+
+###------------------------------------------------------------------------------------------
+###One-sample outcome dataset, combining Dat1+Dat2+Dat4->Dat124 ----------------------------
+#Dat1 交 Dat2 -> Dat12------------------------------------------------
+ID<-match(  Dat1$UKB_sample_ID, Dat2$X )  #按照前者的顺序
+
+Dat2_<-Dat2[ID,]
+Dat12<-cbind(  Dat1$Adiposity_sample_ID ,  Dat2_[,-1]  , Dat1[,-(1:4)]  )
+names(Dat12)[1]<-'V1'
+ncol( Dat2_[,-1] )  # 258 SNPs
+
+#Dat12 交 Dat4 -> Dat124 ----------------------------------------------
+ID<-match(  Dat12$V1, as.numeric(Dat4$eid) )
+
+Dat4_<-Dat4[ID,]
+Dat124<-cbind(  Dat12[,1:259]  ,  Dat4_[,2:3]  ,   Dat12[,-c(1:259)])
+
+#Dat124 remove missing data -> storage as DatY---------------------------
+dim(Dat124)  #488366    461 #ID + 258SNPs + urea + timepoint + nuisance phenotypes
+#names(Dat124)
+
+#removes (ID,Gs,Y, age) NA items
+ID<-  apply(  is.na(Dat124[,1:261]  )  ,  1, sum  )==0
+DatY<-Dat124[ID,]
+dim(DatY)  #350223    461
+
+DatY$Y<-as.numeric(DatY$Y);DatY$age<-as.numeric(DatY$age)
+write.csv(DatY  , paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/DatY.csv'), row.names=F)
+#DatY<-read.csv(paste0('/Users/haodongtian/Documents/files/R new/MPCMR/sim_data_new2/DatY.csv'))
+
+nrow( na.omit(DatY[,1:261]))==nrow(DatY)#TRUE
+
+
 
 
